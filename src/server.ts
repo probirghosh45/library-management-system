5ea17f0ec86af115e92d3a45bc290f0d5ee737a8
Probir Ghosh <probirghosh@outlook.com>
2025-07-28 12:56:17 +0600

    :tada: initial setup done

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors"
+import mongoose from "mongoose";
+import config from "./config";
+
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/",(req,res)=>{
+    res.send({
+        success : true, 
+        message : "This is library management system project" 
+    })
+})
+
+app.listen(5000,()=>{
+    console.log(`Server is running on port ${5000}`)
+})
+
+async function server(){
+    try {
+        // console.log(config)
+        await mongoose.connect(config.database_url!)
+        console.log("Connected to Database")
+    } catch (error) {
+        console.log(`Server error ${server}`)
+    }
+}
+
+server()
