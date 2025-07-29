a3bd5a1bceb944ca90594a6d3309e5f9453d3dc1
Probir Ghosh <probirghosh@outlook.com>
2025-07-29 22:33:22 +0600

    :mag: implemented filtering & sorting for books API

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import Book from "./book.model";
+import { any } from "joi";
+import { SortOrder } from "mongoose";
 
+//create books
 const createBook = async (req : Request , res : Response) =>{
 
     try {
@@ -22,4 +25,41 @@ const createBook = async (req : Request , res : Response) =>{
     }
 }
 
-export {createBook}
+// get all users 
+ const getAllBooks = async (req : Request,res : Response) => {
+    try {
+        // filter books by genre
+        const query : Record<string,any> = {} 
+        const genreQuery = req.query.filter
+        if (genreQuery) {
+            query.genre = genreQuery
+        }
+
+        // sort books
+        const sortBy = (req.query.sortBy as string) || 'createdAt'  // default value = 'createdAt'
+        const sortOrder = req.query.sort === 'asc' ? 1 : -1 // default order = 'descending'
+        const sortOptions : Record<string,SortOrder> = { [sortBy]: sortOrder }
+        
+        // limit books
+        const limit = parseInt (req.query.limit as string) || 10
+
+
+        const books = await Book.find(query).sort(sortOptions).limit(limit)
+        res.status(200).json({
+            success : true,
+            message : "All books fetched successfully",
+            data : books
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : "Failed to fetch books",
+            error : (error as Error).message
+        })
+    }
+ }
+
+
+
+export {createBook,getAllBooks}
--- src/modules/book/book.route.ts
+++ src/modules/book/book.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
-import { createBook } from "./book.controller"
+import { createBook, getAllBooks } from "./book.controller"
 
 
 const bookRoute = Router()
 bookRoute.post("/", createBook)
+bookRoute.get("/", getAllBooks)
 export default bookRoute;
